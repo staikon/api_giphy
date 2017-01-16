d58eb65fcb4fc41688973b762bc836853aaeb93a
Rozenfeld Rémi <Staikon@MacBook-Pro-de-Rozenfeld.local>
2017-01-16 15:47:38 +0100

    recup_gif

--- asset/script/javascript.js
+++ asset/script/javascript.js
@@ -1,18 +1,28 @@
 var domready = function(){
-	var _url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC"
+	var _url = "http://api.giphy.com/v1/gifs/search?q=space&api_key=dc6zaTOxFJmzC"
 
-$.get(_url, function(data){
-	console.log(data);
-	console.log(data.data[0].images.downsized);
+	$.get(_url, function(data){
+		list_gif(data.data);
+	});
+}
 
+var list_gif = function(gifs){
+	for (var i = 0; i<gifs.length; i++) {
+		var _g = gifs[i];
 
+		console.log(_g)
 
-	var img = data.data[0].images.downsized.url;
-	$('#gif img').attr('src', img);
+		// if _g.images doesn't exist
+		if(!_g.images) return;
+		
+		var _tpl = [
+			'<li>',
+				'<img src="' + _g.images.downsized.url + '"/>',
+			'</li>'
+		].join('');
 
-
-});
-
+		$('#gif ul').append(_tpl);
+	}
 }
 
 $(document).ready(domready);
